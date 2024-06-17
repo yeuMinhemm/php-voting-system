d84fa9d5be822097265573a7097fc43ab52b7b81
trong <phambatrong.pbt@gmail.com>
2024-06-17 21:05:44 +0700

    fix: change firstname and lastname order

--- index.php
+++ index.php
@@ -9,32 +9,52 @@
     }
 ?>
 <?php include 'includes/header.php'; ?>
-<body class="hold-transition login-page">
-<div class="login-box">
-  	<div class="login-logo">
-  		<b>Voting System</b>
-  	</div>
-  
-  	<div class="login-box-body">
-    	<p class="login-box-msg">Sign in to start your session</p>
 
-    	<form action="login.php" method="POST">
-      		<div class="form-group has-feedback">
-        		<input type="text" class="form-control" name="voter" placeholder="Voter's ID" required>
-        		<span class="glyphicon glyphicon-user form-control-feedback"></span>
-      		</div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" name="password" placeholder="Password" required>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-      		<div class="row">
-    			<div class="col-xs-4">
-          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
-        		</div>
-      		</div>
-    	</form>
-  	</div>
-  	<?php
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <b>Voting System</b>
+        </div>
+
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+
+            <form
+                action="login.php"
+                method="POST"
+            >
+                <div class="form-group has-feedback">
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="voter"
+                        placeholder="Voter's ID"
+                        required
+                    >
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input
+                        type="password"
+                        class="form-control"
+                        name="password"
+                        placeholder="Password"
+                        required
+                    >
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <button
+                            type="submit"
+                            class="btn btn-primary btn-block btn-flat"
+                            name="login"
+                        ><i class="fa fa-sign-in"></i> Sign In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <?php
   		if(isset($_SESSION['error'])){
   			echo "
   				<div class='callout callout-danger text-center mt20'>
@@ -44,8 +64,9 @@
   			unset($_SESSION['error']);
   		}
   	?>
-</div>
-	
-<?php include 'includes/scripts.php' ?>
+    </div>
+
+    <?php include 'includes/scripts.php' ?>
 </body>
+
 </html>
