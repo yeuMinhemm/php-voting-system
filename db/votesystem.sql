6ed9590013a28661c9e7b279371688c150ee5242
trong <phambatrong.pbt@gmail.com>
2024-06-18 10:20:23 +0700

    fix: update db

--- db/votesystem.sql
+++ db/votesystem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 07:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.15
+-- Host: localhost
+-- Generation Time: Jun 18, 2024 at 03:16 AM
+-- Server version: 11.3.2-MariaDB
+-- PHP Version: 8.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,20 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(60) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `photo` varchar(150) NOT NULL,
   `created_on` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
-(1, 'harie', 'Admin@123', 'Hariharan', 'Elancheliyan', 'facebook-profile-image.jpeg', '2018-04-02');
+(1, 'harie', 'Admin@123', 'Hariharan', 'Elancheliyan', 'facebook-profile-image.jpeg', '2018-04-02'),
+(2, 'trong', '$2y$10$qjm/DkEHtSk99pJevLxqVe.yZqAok/PO11vhCm5pBP25W.3PU2p1y', 'Phạm', 'Trọng', 'man-2_icon-icons.com_55041.png', '2024-04-10');
 
 -- --------------------------------------------------------
 
@@ -54,11 +54,22 @@ INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `pho
 CREATE TABLE `candidates` (
   `id` int(11) NOT NULL,
   `position_id` int(11) NOT NULL,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  `photo` varchar(150) NOT NULL,
-  `platform` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `firstname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`id`, `position_id`, `firstname`, `lastname`, `photo`) VALUES
+(23, 10, 'Morris', 'Garner', ''),
+(24, 10, 'Georgie', 'Barlow', 'candidate2.png'),
+(25, 11, 'Laurel', 'Beck', 'candidate3.jpg'),
+(26, 11, 'Miriam', 'Russell', 'man-2_icon-icons.com_55041.png'),
+(27, 12, 'Selena', 'Stanley', 'candidate6.webp'),
+(28, 12, 'Elliot', 'Robertson', 'candidate4.jpeg');
 
 -- --------------------------------------------------------
 
@@ -71,7 +82,16 @@ CREATE TABLE `positions` (
   `description` varchar(50) NOT NULL,
   `max_vote` int(11) NOT NULL,
   `priority` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `positions`
+--
+
+INSERT INTO `positions` (`id`, `description`, `max_vote`, `priority`) VALUES
+(10, 'Conservatives', 1, 1),
+(11, 'Labour', 1, 2),
+(12, 'Liberal Democrats', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -83,10 +103,21 @@ CREATE TABLE `voters` (
   `id` int(11) NOT NULL,
   `voters_id` varchar(15) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
+  `firstname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `photo` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`id`, `voters_id`, `password`, `firstname`, `lastname`, `photo`) VALUES
+(15, 'iAhgePjuWF9xZ5l', '$2y$10$42IumOf2fAx47rVcN9zPUuLnhsAZKZ10kUcLSjDy6SRojzm78yxiK', 'Phạm Bá', 'Trọng', 'young-smiling-man-adam-avatar-3d-vector-people-character-illustration-cartoon-minimal-style_365941-687.avif'),
+(16, 'dUgYJw4VFfE582O', '$2y$10$TRlTbjFXKDSBLHWX80GvIuD02ghbrx.HgZ6hrPve.zF2zGUYg7JZi', 'Vũ Tùng', 'Quân', 'images (1).jpeg'),
+(17, 'v2owNJxV3TBHsCY', '$2y$10$iJ3KGrhCQIV6yr6kPneI6.zZFNW8.jG/Cy.CPqujFlA/G9khP12lW', 'Nguyễn Đức ', 'Tùng', 'images.jpeg'),
+(18, '2iMOUNgbZSD8qdp', '$2y$10$8l.98qK.JyDhijK9s.Wlduo1OYLXPB3buCvist88FdymlYFnQgswe', 'Đàm Lê Tuấn', 'Vũ', '1600w-879b_Ayodf8.webp'),
+(22, '8SkAM6PJCyzw7rX', '$2y$10$zrp2EnoYUxzz4f0D9WoRH.8IM/DLuyDDHw9hljchyHT2.NS0YmXWe', 'Nguyễn Văn', 'A', '4043260-avatar-male-man-portrait_113269.png');
 
 -- --------------------------------------------------------
 
@@ -99,7 +130,28 @@ CREATE TABLE `votes` (
   `voters_id` int(11) NOT NULL,
   `candidate_id` int(11) NOT NULL,
   `position_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `voters_id`, `candidate_id`, `position_id`) VALUES
+(83, 13, 18, 8),
+(84, 15, 23, 10),
+(85, 15, 25, 11),
+(86, 15, 28, 12),
+(87, 17, 23, 10),
+(88, 17, 26, 11),
+(89, 17, 27, 12),
+(90, 16, 24, 10),
+(91, 16, 26, 11),
+(92, 18, 24, 10),
+(93, 18, 26, 11),
+(94, 18, 28, 12),
+(95, 22, 23, 10),
+(96, 22, 25, 11),
+(97, 22, 27, 12);
 
 --
 -- Indexes for dumped tables
@@ -143,31 +195,31 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT for table `voters`/home/trong/PHP_Voting/logout.php
+-- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
