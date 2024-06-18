540b65ff63c3b1ad5ae3768e201f5bed60e75c3e
trong <phambatrong.pbt@gmail.com>
2024-06-18 10:11:39 +0700

    fix: edit title

--- includes/ballot_modal.php
+++ includes/ballot_modal.php
@@ -1,52 +1,54 @@
 <!-- Preview -->
-<div class="modal fade" id="preview_modal">
+<div
+    class="modal fade"
+    id="preview_modal"
+>
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title">Vote Preview</h4>
+                <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                >
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Vote Preview</h4>
             </div>
             <div class="modal-body">
-              <div id="preview_body"></div>
+                <div id="preview_body"></div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button
+                    type="button"
+                    class="btn btn-default btn-flat pull-left"
+                    data-dismiss="modal"
+                ><i class="fa fa-close"></i> Close</button>
             </div>
         </div>
     </div>
 </div>
 
-<!-- Platform -->
-<div class="modal fade" id="platform">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b><span class="candidate"></b></h4>
-            </div>
-            <div class="modal-body">
-              <p id="plat_view"></p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!-- View Ballot -->
-<div class="modal fade" id="view">
+<div
+    class="modal fade"
+    id="view"
+>
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title">Your Votes</h4>
+                <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                >
+                    <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Phiếu của bạn</h4>
             </div>
             <div class="modal-body">
-              <?php
+                <?php
                 $id = $voter['id'];
                 $sql = "SELECT *, candidates.firstname AS canfirst, candidates.lastname AS canlast FROM votes LEFT JOIN candidates ON candidates.id=votes.candidate_id LEFT JOIN positions ON positions.id=votes.position_id WHERE voters_id = '$id'  ORDER BY positions.priority ASC";
                 $query = $conn->query($sql);
@@ -61,7 +63,11 @@
               ?>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button
+                    type="button"
+                    class="btn btn-default btn-flat pull-left"
+                    data-dismiss="modal"
+                ><i class="fa fa-close"></i> Đóng</button>
             </div>
         </div>
     </div>
